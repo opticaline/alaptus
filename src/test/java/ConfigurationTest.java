ae13b4267c9925f5122c21ef08c5c1d29dede13e
opticaline <opticaline.z@gmail.com>
2014-09-27 02:43:41 +0800

    first commit

---
+++ src/test/java/ConfigurationTest.java
@@ -0,0 +1,34 @@
+import org.apache.commons.configuration2.XMLConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+/**
+ * Created by Nathan on 2014/9/27.
+ */
+public class ConfigurationTest {
+    public static void main(String[] args) throws ConfigurationException, ParserConfigurationException, IOException, SAXException {
+        Parameters params = new Parameters();
+        FileBasedConfigurationBuilder<XMLConfiguration> builder =
+                new FileBasedConfigurationBuilder<XMLConfiguration>(XMLConfiguration.class)
+                        .configure(params.xml()
+                                        .setFileName("config.xml")
+                                /*.setValidating(true)*/);
+
+// This will throw a ConfigurationException if the XML document does not
+// conform to its DTD.
+        XMLConfiguration config = builder.getConfiguration();
+        config.addProperty("test.dir[@name]", "C:\\Temp\\");
+        config.addProperty("test.dir[@name]", "D:\\Data\\");
+        System.out.println(config.getString("test.dir[@name]"));
+        System.out.println(config.getString("Employee.EmployeeType"));
+
+        builder.save();
+    }
+}
