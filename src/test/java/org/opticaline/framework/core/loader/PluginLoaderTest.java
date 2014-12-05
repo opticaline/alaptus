8ad074992eed59b61689d7dae83ef02637a60922
Nathan Zhang <365492386@qq.com>
2014-12-05 18:06:48 +0800

    send to home

---
+++ src/test/java/org/opticaline/framework/core/loader/PluginLoaderTest.java
@@ -0,0 +1,10 @@
+package org.opticaline.framework.core.loader;
+
+/**
+ * Created by Nathan on 2014/12/5.
+ */
+public class PluginLoaderTest {
+    public static void main(String[] args) {
+        
+    }
+}
